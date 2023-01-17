3a147a96b1399833437f5d6fcc02683107c5f996
jueji <1722772741@qq.com>
2023-01-17 11:10:46 +0800

    首页完成

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+// 请求开始前
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+// 请求完成后
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
